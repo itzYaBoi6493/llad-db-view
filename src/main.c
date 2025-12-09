29bed6f21280109a495a6f98dedfe410537a7f1c
Akash Sooraj <akashsooraj14@gmail.com>
2025-12-09 22:52:33 +0530

    db header logic

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
 	int c;
 	bool newfile = false;
 	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
 
 	while ((c = getopt(argc, argv, "nf:")) != -1) {
 		switch (c) {
@@ -41,13 +42,23 @@ int main(int argc, char *argv[]) {
 			printf("Unable to create database file\n");
 			return -1;
 		}
+		if (create_db_header(&dbhdr) == STATUS_ERROR) {
+			printf("Failed to create database header\n");
+			return -1;
+		}
 	} else {
 		dbfd = open_db_file(filepath);
 		if (dbfd == STATUS_ERROR) {
 			printf("Unable to open database file\n");
 			return -1;
 		}
+
+		if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+			printf("Failed to validate database header\n");
+			return -1;
+		}
 	}
+	printf("db header write status = %d\n", output_file(dbfd, dbhdr, NULL));
 	return 0;
 }
 
